3d513771ec32b2a96638b441bd0cdc9f07c63cec
JackLite <jackkdlite@gmail.com>
2022-08-28 13:36:19 +0300

    Init

---
+++ EcsWorldContainer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Core;
+using UnityEngine;
+
+namespace TheTalesOfTwo
+{
+    public class EcsWorldContainer
+    {
+        private static EcsWorldContainer _instance;
+        private Ecs _ecs;
+        public static DataWorld World => _instance._ecs.World;
+        
+        [RuntimeInitializeOnLoadMethod]
+        private static void Startup()
+        {
+            _instance = new EcsWorldContainer();
+            var ecsMono = new GameObject("EcsWorld").AddComponent<EcsWorldMono>();
+            ecsMono.onUpdate = _instance.Update;
+            ecsMono.onFixedUpdate = _instance.FixedUpdate;
+            ecsMono.onLateUpdate = _instance.LateUpdate;
+            ecsMono.onDestroyed = _instance.OnDestroy;
+            _instance._ecs = new Ecs();
+            _instance._ecs.Start();
+        }
+        
+        private void Update()
+        {
+            _ecs.Run();
+        }
+
+        private void FixedUpdate()
+        {
+            _ecs.RunPhysic();
+        }
+
+        private void LateUpdate()
+        {
+            _ecs.PostRun();
+        }
+
+        private void OnDestroy()
+        {
+            _ecs.Destroy();
+        }
+    }
+}
---
+++ EcsWorldMono.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace TheTalesOfTwo
+{
+    public class EcsWorldMono : MonoBehaviour
+    {
+        public Action onUpdate;
+        public Action onFixedUpdate;
+        public Action onLateUpdate;
+        public Action onDestroyed;
+
+        private void Awake()
+        {
+            if (FindObjectOfType<EcsWorldMono>() != this)
+            {
+                DestroyImmediate(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Update()
+        {
+            onUpdate?.Invoke();
+        }
+
+        private void FixedUpdate()
+        {
+            onFixedUpdate?.Invoke();
+        }
+
+        private void LateUpdate()
+        {
+            onLateUpdate?.Invoke();
+        }
+        
+        private void OnDestroy()
+        {
+            onDestroyed?.Invoke();
+        }
+    }
+}
