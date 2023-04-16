d89053115d3f5e3b11234b3f677a5e5fcd2efdad
Sergey Dyuchkov <jackkdlite@gmail.com>
2023-04-16 12:34:44 +0600

    Modules debugger

---
+++ src/Debug/ModuleViewer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ModulesFramework.Modules;
+using UnityEngine;
+
+namespace ModulesFrameworkUnity.Debug
+{
+    public class ModuleViewer : MonoBehaviour
+    {
+        public EcsModule Module { get; private set; }
+
+        public void Init(EcsModule module)
+        {
+            Module = module;
+        }
+
+        private void Update()
+        {
+            name = Module.GetType().Name;
+            if (Module.IsGlobal)
+                name += "|G";
+            if (Module.IsInitialized)
+                name += "|I";
+            else
+                transform.SetAsLastSibling();
+            if (Module.IsActive)
+                name += "|A";
+        }
+    }
+}
