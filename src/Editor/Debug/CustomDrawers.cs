685b007c016238558aa3ef7eb53f21975b49987d
Sergey Dyuchkov <jackkdlite@gmail.com>
2023-05-09 09:43:31 +0600

    Fix drawer

--- src/Editor/Debug/CustomDrawers.cs
+++ src/Editor/Debug/CustomDrawers.cs
@@ -1,5 +1,4 @@
-﻿#if UNITY_EDITOR
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -47,4 +46,3 @@ namespace ModulesFrameworkUnity.Debug
         }
     }
 }
-#endif
