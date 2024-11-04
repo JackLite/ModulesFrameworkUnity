c2598249a63c8b15c9d2b8ac91afe890fa40fd4e
JackLite <jackkdlite@gmail.com>
2024-11-04 16:16:31 +0500

    Improvement of prototype editor

--- src/Editor/Tags/EntityPrototypeWithTagsEditor.cs
+++ src/Editor/Tags/EntityPrototypeWithTagsEditor.cs
@@ -10,18 +10,13 @@ namespace ModulesFrameworkUnity.Tags
     [CustomPropertyDrawer(typeof(EntityPrototypeWithTag))]
     public class EntityPrototypeWithTagsEditor : EntityPrototypeEditor
     {
-        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        protected override void DrawAdditional(SerializedProperty property, VisualElement root)
         {
-            var root = base.CreatePropertyGUI(property);
-
             var tagsProperty = property.FindPropertyRelative(nameof(EntityPrototypeWithTag.tag));
             var tagField = new PropertyField(tagsProperty);
             tagField.AddToClassList("modules-proto--tag-field");
             root.Add(tagField);
             root.styleSheets.Add(Resources.Load<StyleSheet>("EntityPrototypeWithTag"));
-            tagField.SendToBack();
-
-            return root;
         }
     }
 }
