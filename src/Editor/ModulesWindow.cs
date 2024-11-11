dba73f38ae94024f35e22d11b3dcdf7231f8b484
JackLite <jackkdlite@gmail.com>
2024-11-11 17:31:54 +0500

    Fix modules settings window after reopen progect

--- src/Editor/ModulesWindow.cs
+++ src/Editor/ModulesWindow.cs
@@ -9,6 +9,17 @@ namespace ModulesFrameworkUnity
     {
         private ModulesSettings _settings;
 
+        private ModulesSettings Settings
+        {
+            get
+            {
+                if (_settings == null)
+                    LoadSettings();
+
+                return _settings;
+            }
+        }
+
         [MenuItem("Modules/Settings")]
         private static void ShowWindow()
         {
@@ -36,9 +47,9 @@ namespace ModulesFrameworkUnity
 
         private void DrawUseOldDebug()
         {
-            var isUseOldDebug = _settings.useOldDebug;
+            var isUseOldDebug = Settings.useOldDebug;
             var debugContent = new GUIContent("Use old debug", "Turn on to use old debug based on GameObjects");
-            _settings.useOldDebug = EditorGUILayout.Toggle(debugContent, isUseOldDebug);
+            Settings.useOldDebug = EditorGUILayout.Toggle(debugContent, isUseOldDebug);
         }
 
         private void DrawPerformanceSettings()
@@ -46,21 +57,21 @@ namespace ModulesFrameworkUnity
             EditorGUILayout.Space(20);
             EditorGUILayout.BeginVertical();
             DrawTitle("Performance Monitor");
-            var isDebug = _settings.performanceSettings.debugMode;
+            var isDebug = Settings.performanceSettings.debugMode;
             var debugContent = new GUIContent("Debug mode", "If turn off - only panic messages will be logging");
-            _settings.performanceSettings.debugMode = EditorGUILayout.Toggle(debugContent, isDebug);
+            Settings.performanceSettings.debugMode = EditorGUILayout.Toggle(debugContent, isDebug);
 
-            var warn = _settings.performanceSettings.warningAvgFrameMs;
+            var warn = Settings.performanceSettings.warningAvgFrameMs;
             var warnContent = new GUIContent("Warning Threshold",
                 "First threshold. Use it to get warning when code too long but not critical");
-            _settings.performanceSettings.warningAvgFrameMs = EditorGUILayout.FloatField(warnContent, warn);
+            Settings.performanceSettings.warningAvgFrameMs = EditorGUILayout.FloatField(warnContent, warn);
 
-            var panic = _settings.performanceSettings.panicAvgFrameMs;
+            var panic = Settings.performanceSettings.panicAvgFrameMs;
             var panicContent = new GUIContent("Panic Threshold",
                 "This is the critical threshold. " +
                 "You should ignore it when scene loading or some expected going on. " +
                 "It's good to set it to 10-20% from 1/targeted frame rate.");
-            _settings.performanceSettings.panicAvgFrameMs = EditorGUILayout.FloatField(panicContent, panic);
+            Settings.performanceSettings.panicAvgFrameMs = EditorGUILayout.FloatField(panicContent, panic);
 
             EditorGUILayout.EndVertical();
         }
@@ -70,15 +81,15 @@ namespace ModulesFrameworkUnity
             var content = new GUIContent(
                 "Delete empty entities",
                 "If true - when you remove last component from entity it will be destroyed");
-            _settings.deleteEmptyEntities = EditorGUILayout.Toggle(content, _settings.deleteEmptyEntities);
+            Settings.deleteEmptyEntities = EditorGUILayout.Toggle(content, Settings.deleteEmptyEntities);
         }
 
         private void DrawWorldsCount()
         {
             var label = new GUIContent("Worlds count");
-            var currentValue = Mathf.Clamp(_settings.worldsCount, 1, int.MaxValue);
+            var currentValue = Mathf.Clamp(Settings.worldsCount, 1, int.MaxValue);
             var chosen = EditorGUILayout.IntField(label, currentValue);
-            _settings.worldsCount = chosen;
+            Settings.worldsCount = chosen;
         }
 
         private void DrawTitle(string blockTitle)
@@ -90,8 +101,8 @@ namespace ModulesFrameworkUnity
         private void DrawLogType()
         {
             var label = new GUIContent("Log filter");
-            var chosen = EditorGUILayout.EnumFlagsField(label, _settings.logFilter);
-            _settings.logFilter = (LogFilter)chosen;
+            var chosen = EditorGUILayout.EnumFlagsField(label, Settings.logFilter);
+            Settings.logFilter = (LogFilter)chosen;
         }
 
         private void DrawSaveButton()
@@ -103,7 +114,7 @@ namespace ModulesFrameworkUnity
 
             if (GUILayout.Button("Save", style))
             {
-                _settings.Save();
+                Settings.Save();
                 UnityEngine.Debug.Log("Saved!");
             }
         }
@@ -111,8 +122,8 @@ namespace ModulesFrameworkUnity
         private void DrawStartMethod()
         {
             var label = new GUIContent("Start method");
-            var chosen = EditorGUILayout.EnumPopup(label, _settings.startMethod);
-            _settings.startMethod = (StartMethod)chosen;
+            var chosen = EditorGUILayout.EnumPopup(label, Settings.startMethod);
+            Settings.startMethod = (StartMethod)chosen;
         }
     }
 }
